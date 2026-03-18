7c959d34856f80481cc7aa485070f82ec0914c97
Disstopia <cdelasr@naver.com>
2026-03-18 13:44:00 +0900

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -3,12 +3,16 @@ import path from 'path';
 
 export default function handler(req, res) {
   const cookie = req.cookies['dis_auth'];
+
   if (cookie === 'disfur') {
     const html = fs.readFileSync(path.join(process.cwd(), 'catalog.html'), 'utf8');
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Cache-Control', 'no-store');
     return res.status(200).send(html);
   }
+
   const html = fs.readFileSync(path.join(process.cwd(), 'auth.html'), 'utf8');
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.setHeader('Cache-Control', 'no-store');
   return res.status(200).send(html);
 }
